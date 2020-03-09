b8916db3c0fdb99db79f850bc9b6f7b4e9021855
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 00:57:23 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+#array = ["fiddleheads","okra","kohlrabi"]
+#array = ["element", "one more element"]
 
-end
+def oxford_comma(array)
+ if array.size == 1 then
+  commastring = array.join
+  commastring
+ elsif array.size == 2 then
+  commastring = array.join(" and ")
+  commastring
+else 
+ piece = array.last
+ newarray = array 
+ newarray.pop
+ newarray.push("and #{piece}")
+ commastring = newarray.join(", ")
+ commastring
+ end
+ end
+
+# oxford_comma(array)
