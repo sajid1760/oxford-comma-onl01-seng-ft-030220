59cc1874a9404701010a56e3efca7cfd9b34f3a9
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 00:30:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
+array = ["fiddleheads","okra","kohlrabi"]
+
 def oxford_comma(array)
  commaarray = []
  a = array.size 
  b = 0
  a.times do 
-   if b = a - 1 then commaarray += ",and" + array[b]
-   else commaarray += ", " + array[b]
-end
+   if b = a - 1 then 
+     commaarray += ",and" + array[b]
+   else 
+     commaarray += ", " + array[b]
+   end
 puts commaarray
 end
+
+oxford_comma(array)
