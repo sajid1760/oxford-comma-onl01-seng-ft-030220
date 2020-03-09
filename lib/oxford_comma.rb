899816be90582156a22f6d6cec6cd32e1b92ba25
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 00:42:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 array = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
- 
+ puts array.inspect
  newarray = array.pop
- commastring = newarray.join
+ puts newarray.inspect
+ # commastring = newarray.join
  commastring = commastring + ", and " + array.last 
  puts commastring
  end
