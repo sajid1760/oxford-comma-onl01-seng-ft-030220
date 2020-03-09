eafafa6ed81dfaca4ff0287bbd0821a692cb4f23
Sajid Anjum <sajid1760@gmail.com>
2020-03-09 00:56:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,21 @@
 #array = ["fiddleheads","okra","kohlrabi"]
-array = ["element", "one more element"]
+#array = ["element", "one more element"]
 
 def oxford_comma(array)
  if array.size == 1 then
   commastring = array
-  puts commastring
+  commastring
  elsif array.size == 2 then
   commastring = array.join(" and ")
-  puts commastring
+  commastring
 else 
  piece = array.last
  newarray = array 
  newarray.pop
  newarray.push("and #{piece}")
  commastring = newarray.join(", ")
- puts commastring
+ commastring
  end
  end
 
-oxford_comma(array)
+# oxford_comma(array)
